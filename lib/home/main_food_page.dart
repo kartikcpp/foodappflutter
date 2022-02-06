ac0c35f892fb4fa78b6ee6978c19be0368bf2a04
kartik <kartiksaini8881@gmail.com>
2022-02-06 20:53:21 +0530

    initial

---
+++ lib/home/main_food_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class MainFoodPage extends StatefulWidget {
+  MainFoodPage({Key? key}) : super(key: key);
+
+  @override
+  State<MainFoodPage> createState() => _MainFoodPageState();
+}
+
+class _MainFoodPageState extends State<MainFoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

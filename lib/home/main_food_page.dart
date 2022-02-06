22e6383753d2733109160d542f88c5ac044821b7
kartik <kartiksaini8881@gmail.com>
2022-02-06 21:17:52 +0530

    header

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class MainFoodPage extends StatefulWidget {
   MainFoodPage({Key? key}) : super(key: key);
 
@@ -9,6 +10,33 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top:45,bottom: 15),
+            padding: EdgeInsets.only(left: 20,right: 20),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [Text('India'), Text('India')],
+                  ),
+                  Container(
+                    child: Icon(Icons.search,color: Colors.white,),
+                    width: 45,
+                    height: 45,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.amberAccent),
+                  )
+                ]),
+          ),
+
+        
+        
+        ],
+      ),
+    );
   }
 }
